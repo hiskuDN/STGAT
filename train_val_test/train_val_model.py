234692d2181704ef156fa7a53019822a0ed67108
Hiskias <hiskias.melke1@gmail.com>
2024-08-13 13:59:02 +0900

    fix: tensor size not matching when using random_cutout

--- train_val_test/train_val_model.py
+++ train_val_test/train_val_model.py
@@ -138,10 +138,11 @@ def train_classifier(
             
             # random cutout
             aug_inputs = random_cutout(inputs, 1)
-            aug_targets = labels
             
             inputs = torch.cat((inputs, aug_inputs), dim=0)
-            targets = torch.cat((labels, aug_targets), dim=0)
+            targets = torch.cat((labels, labels), dim=0)
+            
+            labels = targets
         
         elif args.label_smoothing_num != 0 or args.loss == "cross_entropy_naive":
             targets = to_onehot(args.class_num, labels, args.label_smoothing_num)
